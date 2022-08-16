dea42d604db402091110cad9d5f346487fb09c8a
FedericoPelanda <110478872+FedericoPelanda@users.noreply.github.com>
2022-08-16 11:04:34 -0300

    com

---
+++ js/products.js
@@ -0,0 +1,44 @@
+//Array donde se cargarán los datos recibidos:
+let autosArray = [];
+
+//Función que recibe el array, y los muestra en pantalla por medio del DOM
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.products.length; i++){ 
+        let category = array.products[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name + ` - ` + category.currency + ` ` + category.cost +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
+        }
+    });
+});
