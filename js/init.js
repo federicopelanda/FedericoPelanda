f9e3a19d5000c53987572b88c235ace19be7421e
FedericoPelanda <110478872+FedericoPelanda@users.noreply.github.com>
2022-09-06 09:23:05 -0300

    4234234

--- js/init.js
+++ js/init.js
@@ -79,6 +79,21 @@ function onSignIn(googleUser) {
   console.log("Email: " + profile.getEmail());*/
   // The ID token you need to pass to your backend:
 
+  function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  }
+
   let users = {};
   
   users.id = profile.getId()
