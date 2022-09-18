428f341e7a38ce543c5f9f0c030cc1b9b317a2f4
FedericoPelanda <110478872+FedericoPelanda@users.noreply.github.com>
2022-09-18 12:32:39 -0300

    entrega3

--- js/login.js
+++ js/login.js
@@ -3,8 +3,8 @@ function login(){
     password = document.getElementById('password');
 
     if(email.value !== "" && password.value !== ""){
-        localStorage.setItem('user',JSON.stringify(email.value));
-        location.href = "index.html"
+        localStorage.setItem('user',JSON.parse(JSON.stringify(email.value)));
+        location.href = "/index.html"
     } else if(email.value === ""){
         email.classList.add('is-invalid'); 
     }if(password.value === ""){
@@ -25,23 +25,3 @@ document.addEventListener('DOMContentLoaded', () =>{
       login();
     })
 })
-
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    
-    localStorage.setItem('user',"prueba");
-    location.href = "index.html"
-
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-
-}
